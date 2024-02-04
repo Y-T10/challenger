818a8284aff31ab18df5c2932db7f8d15328a08b
Y-T10 <t06t06ca2@gmail.com>
2024-02-05 00:32:55 +0900

    イテレータクラスをソースファイルに移動した

--- src/challenger_event.cpp
+++ src/challenger_event.cpp
@@ -1,5 +1,46 @@
 #include "challenger/challenger_event.hpp"
 
+namespace {
+    /**
+     * @brief SDLのイベントキューを扱うイテレータ
+     * @tparam does_remove インクリメント時に現在の要素を破棄するか
+     */
+    template <bool does_remove>
+    struct event_queue_iterator {
+        using value_type = SDL_Event;
+        using pointer = SDL_EventQueueElement;
+        using reference = std::add_lvalue_reference_t<value_type>;
+        using iterator_category = std::input_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+
+        SDL_EventQueueElement iter;
+
+        event_queue_iterator(pointer p):
+        iter(p){}
+
+        reference operator*() const noexcept {
+            return *(SDL_GetEvent(iter));
+        }
+
+        event_queue_iterator& operator++() noexcept {
+            iter = SDL_ForwardElement(iter, does_remove);
+            return *this;
+        }
+
+        event_queue_iterator operator++(int) noexcept {
+            event_queue_iterator ret(iter);
+            ++*this;
+            return ret;
+        }
+
+        bool operator==(const event_queue_iterator& rval) noexcept {
+            return iter == rval.iter;
+        }
+    };
+
+    static_assert(std::input_iterator<event_queue_iterator<true>>);
+    static_assert(std::input_iterator<event_queue_iterator<false>>);
+}
 
 inline std::vector<SDL_Event> challenger::FetchAllEvents() noexcept {
     SDL_LockEventQueue();
