1c88a5dc9aac87b299198b3315a98ca30e2b6704
Y-T10 <t06t06ca2@gmail.com>
2024-05-08 22:40:20 +0900

    不要な関数定義を削除

--- include/challenger/challenger_video.hpp
+++ include/challenger/challenger_video.hpp
@@ -12,16 +12,6 @@ namespace challenger {
     using Window = SDL_ptr<SDL_Window, SDL_DestroyWindow>;
     using Surface = SDL_ptr<SDL_Surface, SDL_DestroySurface>;
 
-    template<class ptr_type, auto Func, class ...Args>
-    const ptr_type Create(Args&& ...args) noexcept {
-        constexpr bool chechFunc = std::is_invocable_r_v<typename ptr_type::pointer, decltype(Func), Args...>;
-        static_assert(chechFunc, "Func or ptr_type is wrong.");
-        if constexpr (chechFunc) {
-            return ptr_type(Func(args...));
-        }
-        return nullptr;
-    };
-
     inline const Window CreateWindow(const std::string& title, int w, int h, Uint32 flags) noexcept {
         return Window(SDL_CreateWindow(title.c_str(), w, h, flags));
     };
