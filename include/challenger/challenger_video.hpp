e459efefe37f6f8e3c082eaf9c1818df7c5ae17d
Y-T10 <t06t06ca2@gmail.com>
2024-05-08 23:02:17 +0900

    Surfaceの定義を削除

--- include/challenger/challenger_video.hpp
+++ include/challenger/challenger_video.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
-#include <type_traits>
 #include <string>
 
 #include "challenger_memory.hpp"
 
-#include "SDL3/SDL_surface.h"
 #include "SDL3/SDL_video.h"
 
 namespace challenger {
     using Window = SDL_ptr<SDL_Window, SDL_DestroyWindow>;
-    using Surface = SDL_ptr<SDL_Surface, SDL_DestroySurface>;
 
     inline const Window CreateWindow(const std::string& title, int w, int h, Uint32 flags) noexcept {
         return Window(SDL_CreateWindow(title.c_str(), w, h, flags));
